80dd71b2f1687cf31d5d89a328de2a62b9676aa9
Shaken-Dan Bolatuly <shd.cs.ch@gmail.com>
2023-05-18 18:42:17 +0600

    html and python files bridges are done, looks like it's a backender's job looks like

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Import Flask and wtforms"""
 from flask.views import MethodView
-from wtforms import Form
-from flask import Flask
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template
 
 # Flask is an object that represents a web app
 
@@ -11,17 +11,26 @@ app = Flask(__name__)
 class CalorieHomePage(MethodView):
 
     def get(self):
-        return "Hello"
+        return render_template('index.html')
 
 
 class CalorieFormPage(MethodView):
 
     def get(self):
-        return print('Form Page')
+        # initialize CalorieForm instance
+        calorie_form = CalorieForm()
+        return render_template('calorie_form_page.html', cal_form=calorie_form)
 
 
 class CalorieForm(Form):
-    pass
+    weight = StringField('Weight: ')
+    height = StringField('Height: ')
+    age = StringField('Age: ')
+
+    city = StringField('City: ')
+    country = StringField('Country: ')
+
+    button = SubmitField('Calculate')
 
 
 app.add_url_rule('/', view_func=CalorieHomePage.as_view('home_page'))
